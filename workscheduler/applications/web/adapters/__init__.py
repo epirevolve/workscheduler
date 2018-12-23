7b3d889a4afcd89324de0f7fb2a3ea36b96ef84a
ujucomtonin <45261334+ujucomtonin@users.noreply.github.com>
2018-12-23 21:56:45 +0900

    re-order by alphabet.

--- workscheduler/applications/web/adapters/__init__.py
+++ workscheduler/applications/web/adapters/__init__.py
@@ -3,5 +3,5 @@
 from .auth_command_adapter import AuthCommandAdapter
 from .belong_command_adapter import BelongCommandAdapter
 from .operator_command_adapter import OperatorCommandAdapter
-from .user_command_adapter import UserCommandAdapter
 from .team_command_adapter import TeamCommandAdapter
+from .user_command_adapter import UserCommandAdapter
