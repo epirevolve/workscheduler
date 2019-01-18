9866b263e6ed6f6fb7bde6a8b52795c256324741
yukihiro <epirevolve@gmail.com>
2019-01-18 22:50:01 +0900

    modify import

--- workscheduler/domains/models/scheduler/__init__.py
+++ workscheduler/domains/models/scheduler/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from .basic_option import BasicOptions
-from .calendar import Calendar
-from .calendar_day import CalendarDay
-from .calendar_day_detail import CalendarDayDetail
-from .specific_vacation import SpecificVacation
 from .work_category import WorkCategory
+from .specific_vacation import SpecificVacation
+from .basic_option import BasicOptions
+
+from .calendar_day_detail import CalendarDayDetail
+from .calendar_day import CalendarDay
+from .calendar import Calendar
+
