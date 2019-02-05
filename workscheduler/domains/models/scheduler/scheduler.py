0221e004f3326c1192f713415870bf68daadb01c
yukihiro <epirevolve@gmail.com>
2019-02-05 18:22:40 +0900

    add eart package

--- workscheduler/domains/models/scheduler/scheduler.py
+++ workscheduler/domains/models/scheduler/scheduler.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 
+from eart.selections import (
+    EliteSelection, TournamentSelection
+)
+from eart.mutations import (
+    WholeMutation, InvertMutation, TranslocateMutation
+)
+from eart.crossovers import (
+    CircuitCrossover, OrderlyCrossover
+)
 from eart import (
-    Individual, Genetic
+    Genetic, MarriageSelection, TransitionSelection,
+    Mutation, Crossover
 )
 
 
@@ -9,6 +19,47 @@ class Scheduler:
     def __init__(self, affiliation_id, calendar_id):
         self._affiliation_id = affiliation_id
         self._calendar_id = calendar_id
-
-    def run(self):
+    
+    def _evaluate(self):
         pass
+    
+    @staticmethod
+    def _build_marriage_selection():
+        marriage_selection = MarriageSelection()
+        marriage_selection.add(EliteSelection(), 0.05)
+        marriage_selection.add(TournamentSelection(group_size=3))
+        marriage_selection.compile()
+        return marriage_selection
+    
+    @staticmethod
+    def _build_transition_selection(population_size):
+        transition_selection = TransitionSelection(const_population_size=population_size)
+        transition_selection.add(EliteSelection(), 0.05)
+        transition_selection.add(TournamentSelection(group_size=3))
+        transition_selection.compile()
+        return transition_selection
+    
+    @staticmethod
+    def _build_mutation():
+        mutation = Mutation(proliferate_mutation=True)
+        mutation.add(WholeMutation(), 0.05)
+        mutation.add(InvertMutation())
+        mutation.add(TranslocateMutation())
+        mutation.compile()
+        return mutation
+    
+    @staticmethod
+    def _build_crossover():
+        crossover = Crossover()
+        crossover.add(CircuitCrossover())
+        crossover.add(OrderlyCrossover())
+        crossover.compile()
+        return crossover
+    
+    def run(self):
+        genetic = Genetic(evaluation=self._evaluate, gene_kind='', generation_size=500)
+        genetic.marriage_selection = self._build_marriage_selection()
+        genetic.transition_selection = self._build_transition_selection(genetic.population_size)
+        genetic.mutation = self._build_mutation()
+        genetic.crossover = self._build_crossover()
+        return genetic.run()
