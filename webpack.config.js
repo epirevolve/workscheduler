bc9af17ac21d433cd789c082751e5caf34f1706a
yukihiro <epirevolve@gmail.com>
2019-05-31 23:16:38 +0900

    refactor month select on schedule page

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
-const _utilPath = './workscheduler/applications/web/util/statics/js';
-const _path = (folder) => `./workscheduler/applications/web/models/${folder}/statics/js`;
+const _utilPath = './workscheduler/source/applications/web/util/statics/js';
+const _path = (folder) => `./workscheduler/source/applications/web/models/${folder}/statics/js`;
 const _userPath = _path('user');
 const _operatorPath = _path('operator');
 const _schedulerPath = _path('scheduler');
@@ -11,23 +11,23 @@ module.exports = {
     mode: 'production',
 
     entry: {
-        'workscheduler/applications/web/util/statics/js/layout': path.join(_utilPath, '/src/layout/index.jsx'),
-        'workscheduler/applications/web/util/statics/js/main-menu': path.join(_utilPath, '/src/menu/index.jsx'),
-        'workscheduler/applications/web/models/user/statics/js/auth': path.join(_userPath, '/src/auth/index.jsx'),
-        'workscheduler/applications/web/models/user/statics/js/main-teams': path.join(_userPath, '/src/main-teams/index.jsx'),
-        'workscheduler/applications/web/models/user/statics/js/user': path.join(_userPath, '/src/user/index.jsx'),
-        'workscheduler/applications/web/models/user/statics/js/users': path.join(_userPath, '/src/users/index.jsx'),
-        'workscheduler/applications/web/models/operator/statics/js/operator': path.join(_operatorPath, '/src/operator/index.jsx'),
-        'workscheduler/applications/web/models/operator/statics/js/operators': path.join(_operatorPath, '/src/operators/index.jsx'),
-        'workscheduler/applications/web/models/operator/statics/js/skills': path.join(_operatorPath, '/src/skills/index.jsx'),
-        'workscheduler/applications/web/models/scheduler/statics/js/request-non-public': path.join(_schedulerPath, '/src/request-non-public/index.jsx'),
-        'workscheduler/applications/web/models/scheduler/statics/js/request-public': path.join(_schedulerPath, '/src/request-public/index.jsx'),
-        'workscheduler/applications/web/models/scheduler/statics/js/scheduler-menu': path.join(_schedulerPath, '/src/scheduler-menu/index.jsx'),
-        'workscheduler/applications/web/models/scheduler/statics/js/scheduler-monthly-setting': path.join(_schedulerPath, '/src/scheduler-monthly-setting/index.jsx'),
-        'workscheduler/applications/web/models/scheduler/statics/js/scheduler-basic-setting': path.join(_schedulerPath, '/src/scheduler-basic-setting/index.jsx'),
-        'workscheduler/applications/web/models/scheduler/statics/js/scheduler-yearly-setting': path.join(_schedulerPath, '/src/scheduler-yearly-setting/index.jsx'),
-        'workscheduler/applications/web/models/schedule/statics/js/schedule-operator': path.join(_schedulePath, '/src/schedule-operator/index.jsx'),
-        'workscheduler/applications/web/models/schedule/statics/js/schedule-admin': path.join(_schedulePath, '/src/schedule-admin/index.jsx'),
+        'workscheduler/source/applications/web/util/statics/js/layout': path.join(_utilPath, '/src/layout/index.jsx'),
+        'workscheduler/source/applications/web/util/statics/js/main-menu': path.join(_utilPath, '/src/menu/index.jsx'),
+        'workscheduler/source/applications/web/models/user/statics/js/auth': path.join(_userPath, '/src/auth/index.jsx'),
+        'workscheduler/source/applications/web/models/user/statics/js/main-teams': path.join(_userPath, '/src/main-teams/index.jsx'),
+        'workscheduler/source/applications/web/models/user/statics/js/user': path.join(_userPath, '/src/user/index.jsx'),
+        'workscheduler/source/applications/web/models/user/statics/js/users': path.join(_userPath, '/src/users/index.jsx'),
+        'workscheduler/source/applications/web/models/operator/statics/js/operator': path.join(_operatorPath, '/src/operator/index.jsx'),
+        'workscheduler/source/applications/web/models/operator/statics/js/operators': path.join(_operatorPath, '/src/operators/index.jsx'),
+        'workscheduler/source/applications/web/models/operator/statics/js/skills': path.join(_operatorPath, '/src/skills/index.jsx'),
+        'workscheduler/source/applications/web/models/scheduler/statics/js/request-non-public': path.join(_schedulerPath, '/src/request-non-public/index.jsx'),
+        'workscheduler/source/applications/web/models/scheduler/statics/js/request-public': path.join(_schedulerPath, '/src/request-public/index.jsx'),
+        'workscheduler/source/applications/web/models/scheduler/statics/js/scheduler-menu': path.join(_schedulerPath, '/src/scheduler-menu/index.jsx'),
+        'workscheduler/source/applications/web/models/scheduler/statics/js/scheduler-monthly-setting': path.join(_schedulerPath, '/src/scheduler-monthly-setting/index.jsx'),
+        'workscheduler/source/applications/web/models/scheduler/statics/js/scheduler-basic-setting': path.join(_schedulerPath, '/src/scheduler-basic-setting/index.jsx'),
+        'workscheduler/source/applications/web/models/scheduler/statics/js/scheduler-yearly-setting': path.join(_schedulerPath, '/src/scheduler-yearly-setting/index.jsx'),
+        'workscheduler/source/applications/web/models/schedule/statics/js/schedule-operator': path.join(_schedulePath, '/src/schedule-operator/index.jsx'),
+        'workscheduler/source/applications/web/models/schedule/statics/js/schedule-admin': path.join(_schedulePath, '/src/schedule-admin/index.jsx'),
     },
 
     output: {
@@ -52,7 +52,7 @@ module.exports = {
                     loader: 'file-loader',
                     options: {
                         name: '[name].[ext]',
-                        outputPath: 'workscheduler/applications/web/util/statics/css/webfonts/'
+                        outputPath: 'workscheduler/source/applications/web/util/statics/css/webfonts/'
                     }
                 }]
             }
@@ -65,10 +65,10 @@ module.exports = {
         modules: [
             'node_modules',
             path.resolve('.'),
-            path.join(path.resolve('.'), 'workscheduler/applications/web/util/statics/js'),
-            path.join(path.resolve('.'), 'workscheduler/applications/web/util/statics/js/src/functions'),
-            path.join(path.resolve('.'), 'workscheduler/applications/web/util/statics/js/src/components'),
-            path.join(path.resolve('.'), 'workscheduler/applications/web/util/statics/css')
+            path.join(path.resolve('.'), 'workscheduler/source/applications/web/util/statics/js'),
+            path.join(path.resolve('.'), 'workscheduler/source/applications/web/util/statics/js/src/functions'),
+            path.join(path.resolve('.'), 'workscheduler/source/applications/web/util/statics/js/src/components'),
+            path.join(path.resolve('.'), 'workscheduler/source/applications/web/util/statics/css')
         ],
         extensions: ['.js', '.jsx', '.css']
     },
@@ -78,13 +78,13 @@ module.exports = {
             cacheGroups: {
                 common: {
                     test: /node_modules/,
-                    name: 'workscheduler/applications/web/util/statics/js/vendor/common',
+                    name: 'workscheduler/source/applications/web/util/statics/js/vendor/common',
                     chunks: 'initial',
                     enforce: true
                 },
                 utilCommon: {
-                    test: 'workscheduler/applications/web/util/statics/js/src/functions',
-                    name: 'workscheduler/applications/web/util/statics/js/util-common',
+                    test: 'workscheduler/source/applications/web/util/statics/js/src/functions',
+                    name: 'workscheduler/source/applications/web/util/statics/js/util-common',
                     chunks: 'initial',
                     enforce: true
                 }
